f971a050611906f52c983cc7e6341ac2fe9e7b0c
Rob Tong <robtong21@gmail.com>
2017-03-07 11:27:33 -0600

    added section 7

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,19 @@ const router = express.Router();
 // could use one line instead: const router = require('express').Router();
 const tweetBank = require('../tweetBank');
 
+router.get('/users/:name', function(req, res) {
+  var name = req.params.name;
+  var list = tweetBank.find( {name: name} );
+  res.render( 'index', { tweets: list } );
+});
+
+router.get('/tweets/:id', function(req, res) {
+  var id = Number(req.params.id);
+  console.log(typeof id)
+  var list = tweetBank.find( {id: id} );
+  res.render( 'index', { tweets: list } );
+});
+
 router.get('/', function(req, res) {
     let tweets = tweetBank.list();
     res.render('index', { tweets: tweets });
