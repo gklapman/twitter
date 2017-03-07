ae21f161aa902441c4a256a92117a287dfb4c321
gklapman <gabiklapman@gmail.com>
2017-03-07 16:45:25 -0600

    twitter functioning without socket

--- app.js
+++ app.js
@@ -1,30 +1,41 @@
-const express = require('express');
-const morgan = require('morgan');
-const nunjucks = require('nunjucks');
-const app = express(); // creates an instance of an express application
-const tweetBank = require('./tweetBank');
-const routes = require('./routes');
+ const express = require('express');
+ const morgan = require('morgan');
+ const nunjucks = require('nunjucks');
+ const app = express(); // creates an instance of an express application
+ const tweetBank = require('./tweetBank');
+ const routes = require('./routes');
+ const bodyParser = require('body-parser');
+ var socketio = require('socket.io');
 
 
-app.set('view engine', 'html'); // have res.render work with html files
-app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
-nunjucks.configure('views', { noCache: true }); // point nunjucks to the proper directory for templates
+ app.set('view engine', 'html'); // have res.render work with html files
+ app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+ nunjucks.configure('views', { noCache: true }); // point nunjucks to the proper directory for templates
 
-// app.use('/', function(req, res) {
-//     // in some file that is in the root directory of our application... how about app.js?
+ // app.use('/', function(req, res) {
+ //     // in some file that is in the root directory of our application... how about app.js?
 
-//     nunjucks.configure('views', { noCache: true });
-//     nunjucks.render('index.html', function(err, output) {
-//         console.log(output);
-//         res.send(output)
-//     });
+ //     nunjucks.configure('views', { noCache: true });
+ //     nunjucks.render('index.html', function(err, output) {
+ //         console.log(output);
+ //         res.send(output)
+ //     });
 
-// })
+ // })
 
-app.use(express.static('public'));
 
-app.use('/', routes);
 
-app.listen(3000, function() {
-    console.log("Server listening...")
-});
+ app.use(bodyParser.urlencoded({ extended: false }))
+
+ app.use(bodyParser.json());
+
+ app.use(express.static('public'));
+
+ var server = app.listen(3000, function() {
+     console.log("Server listening...")
+ });
+
+
+ var io = socketio.listen(server);
+
+ app.use('/', routes(io));
--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,56 @@
+
 const express = require('express');
 const morgan = require('morgan');
 const router = express.Router();
 // could use one line instead: const router = require('express').Router();
 const tweetBank = require('../tweetBank');
-
-router.get('/users/:name', function(req, res) {
-  var name = req.params.name;
-  var list = tweetBank.find( {name: name} );
-  res.render( 'index', { tweets: list } );
-});
-
-router.get('/tweets/:id', function(req, res) {
-  var id = Number(req.params.id);
-  console.log(typeof id)
-  var list = tweetBank.find( {id: id} );
-  res.render( 'index', { tweets: list } );
-});
-
-router.get('/', function(req, res) {
-    let tweets = tweetBank.list();
-    res.render('index', { tweets: tweets });
-});
-
-router.use(function(req, res, next) {
-    console.log(req.method)
-    console.log(req.path)
-    next();
-})
-
-router.use(morgan('dev'))
+const bodyParser = require('body-parser');
 
 
-module.exports = router;
+
+
+module.exports = function routes(io) {
+
+    router.get('/users/:name', function(req, res) {
+        var name = req.params.name;
+        var tweets = tweetBank.find({ name: name });
+        res.render('index', { tweets: tweets, name: name, showForm: true });
+    });
+
+    router.get('/tweets/:id', function(req, res) {
+        var id = Number(req.params.id);
+        console.log(typeof id)
+        var tweets = tweetBank.find({ id: id });
+        res.render('index', { tweets: tweets });
+    });
+
+    router.get('/', function(req, res) {
+        let tweets = tweetBank.list();
+        res.render('index', { tweets: tweets,
+ showForm: true });
+    });
+
+    router.use(function(req, res, next) {
+        console.log(req.method)
+        console.log(req.path)
+        next();
+    })
+
+    router.post('/tweets', function(req, res) {
+        var name = req.body.name;
+        var text = req.body.text;
+        tweetBank.add(name, text);
+        io.sockets.emit('newTweet', {
+            name: name,
+            text: text
+        })
+        res.redirect('/');
+    });
+
+    router.use(morgan('dev'));
+
+    return router;
+}
 
 
 
